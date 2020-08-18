900d6be5e3917eed238b07270c2d601829456d0b
Alejandro Sánchez <alejandroingelec19@gmail.com>
2020-08-17 22:16:24 -0400

    se modifica reporte de orden de entrega agregando terminos en ingles

--- stock_extended/models/stock_picking.py
+++ stock_extended/models/stock_picking.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-
+import logging
 
 from odoo import api, fields, models, _ 
 
-
+_logger = logging.getLogger('__name__')
 
 
 
@@ -16,3 +16,7 @@ class StockPicking(models.Model):
     security_bag = fields.Boolean(string='Security Bag')
     insurance = fields.Boolean(string='Insurance')
     parcel_service = fields.Char(string='Parcel service')
+
+    # @api.depends('origin')
+    # def _unit_price(self):
+    #     unit_price = self.env['sale.order'].search()
