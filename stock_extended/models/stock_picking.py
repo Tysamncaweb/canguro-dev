f7e16246b0939925297232749163ebc3be75e1b3
Alejandro Sánchez <alejandroingelec19@gmail.com>
2020-08-19 13:21:00 -0400

    Se ajusta nuevamente el formato del reporte de Ordenes de Pedidos

--- stock_extended/models/stock_picking.py
+++ stock_extended/models/stock_picking.py
@@ -3,6 +3,7 @@
 import logging
 
 from odoo import api, fields, models, _ 
+from odoo.exceptions import ValidationError 
 
 _logger = logging.getLogger('__name__')
 
@@ -16,3 +17,12 @@ class StockPicking(models.Model):
     security_bag = fields.Boolean(string='Security Bag')
     insurance = fields.Boolean(string='Insurance')
     parcel_service = fields.Char(string='Parcel service')
+
+
+    @api.constrains('note')
+    def _note_limited(self):
+        if len(self.note) > 89:
+            raise ValidationError(_("Field note cannot be contain more than 89 characters.!"))
+
+
+
