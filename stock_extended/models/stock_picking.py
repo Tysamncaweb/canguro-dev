103ada98dd5c3133ccc06b0ef26e96c014f27716
Alejandro Sánchez <alejandroingelec19@gmail.com>
2020-08-26 18:56:17 -0400

    Se agrega el reporte orden de pedido al boton print que falta traducirlo. También es importante mencionar que se agrego el método que invoca y todo lo demás.

--- stock_extended/models/stock_picking.py
+++ stock_extended/models/stock_picking.py
@@ -27,6 +27,12 @@ class StockPicking(models.Model):
             pass
 
 
+    def do_print_picking(self):
+        _logger.info("\n\n Aqui estoy\n\n\n")
+        self.write({'printed': True})
+        _logger.info("\n\n self %s \n\n", self)
+        return self.env.ref('stock.action_report_delivery').report_action(self)
+
 
 class StockShippingReport(models.Model):
     _name = "stock.shipping.report"
