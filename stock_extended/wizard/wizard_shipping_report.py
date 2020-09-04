84e7e0144fdff96d24f02bc796b985f569055533
Alejandro Sánchez <alejandroingelec19@gmail.com>
2020-09-03 21:53:19 -0400

    Se agrega términos faltantes en la traducción, se agrega tipo de papel para el reporte de envíos o shipping report y finalmente se agregan validaciones para las fechas en el wizard.

--- stock_extended/wizard/wizard_shipping_report.py
+++ stock_extended/wizard/wizard_shipping_report.py
@@ -2,7 +2,8 @@
 
 import logging
 
-from odoo import api, models, fields
+from odoo import api, models, fields, _
+from odoo.exceptions import ValidationError
 
 
 _logger = logging.getLogger('__name__')
@@ -19,6 +20,13 @@ class StockShippingReportWizard(models.TransientModel):
     
 
     def shipping_report(self):
+        
+        _logger.info("\n\n type %s \n\n", type(self.date_from))
+        if self.date_from > self.date_to:
+            raise ValidationError(_("The start date cannot be greater than the end date"))
+        elif (self.date_from == False) or (self.date_to == False):
+            raise ValidationError(_("Fields date cannot be empty."))
+
         data = {
             'model': 'stock.shipping.report.wizard',
             'form': self.read()[0]
