f63543e35938b6878d97435107a30c3841d3359c
Alejandro Sánchez <alejandroingelec19@gmail.com>
2020-11-24 19:30:07 -0400

    Se agrega funcion para que no permita tener el campo de referencia del producto en blanco tampoco permitira duplicar.

--- stock_extended/models/product_template.py
+++ stock_extended/models/product_template.py
@@ -2,6 +2,7 @@
 
 
 from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 
@@ -9,7 +10,10 @@ from odoo import api, fields, models, _
 class ProductTemplate(models.Model):
     _inherit='product.template'
 
-    #default_code = fields
+    @api.constrains('default_code')
+    def unique_code(self):
+        if not self.default_code:
+            raise ValidationError(_("The default code cannot be blank"))
 
     _sql_constraints = [
             ('default_code_uniq', 'unique (default_code)', 'Remember the code it\'s unique by each product')
