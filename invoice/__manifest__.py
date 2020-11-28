7f21834131693863c43bd0e8b77e5a67cd58aa70
Alejandro Sánchez <alejandroingelec19@gmail.com>
2020-11-28 18:36:01 -0400

    Se elimina encabezado y campo de iva

---
+++ invoice/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+
+{
+        'name': 'Account Invoices',
+        'version': '0.1',
+        'author': 'Tysamnca',
+        'category': 'Accounting/Accounting',
+        'depends': ['account'],
+        'data': [
+            'views/template_report_invoice_document.xml',
+            ],
+        'installable': True,
+        'application': True,  
+        
+        }
